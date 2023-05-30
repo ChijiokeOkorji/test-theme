2ed8c780fdfa9aee4c8b0582927d10eb8c6b6a19
Chijioke Okorji <chijiokeokorji@gmail.com>
2023-05-30 13:03:13 +0100

    Updated styles

--- javascripts/discourse/api-initializers/avatar-size.js
+++ javascripts/discourse/api-initializers/avatar-size.js
@@ -1,16 +1,16 @@
-// import { withPluginApi } from "discourse/lib/plugin-api";
+import { withPluginApi } from "discourse/lib/plugin-api";
 
-// export default {
-//   name: "avatar-size",
+export default {
+  name: "avatar-size",
   
-//   initialize(container) {
-//     withPluginApi("0.8.7", (api) => {
-//       const site = api.container.lookup("site:main");
-//       // Change avatar size on desktop
-//       if (!site.mobileView) {
-//         // api.changeWidgetSetting("post-avatar", "size", 60);
-//         api.changeWidgetSetting("post-avatar", "size", 360);
-//       }
-//     });
-//   },
-// };
+  initialize(container) {
+    withPluginApi("0.8.7", (api) => {
+      const site = api.container.lookup("site:main");
+      // Change avatar size on desktop
+      if (!site.mobileView) {
+        // api.changeWidgetSetting("post-avatar", "size", 60);
+        api.changeWidgetSetting("post-avatar", "size", 360);
+      }
+    });
+  },
+};
